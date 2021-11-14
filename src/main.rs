c7a26f31952a4c66fbbee2fee094254f6c3672d7
KokubunKazuya <kazup1125@me.com>
2021-11-14 23:53:20 +0900

    mecabを使用して認識できた2文字以上の単語をカウントし、その数が最も多い角度を正とする

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+use mecab::Tagger;
 use opencv::core::{MatTraitManual,
                    Vector,
                    rotate,
@@ -19,7 +21,7 @@ fn main() {
 
     let rotate_code_list = vec![NO_ROTATE, ROTATE_90_CLOCKWISE, ROTATE_180, ROTATE_90_COUNTERCLOCKWISE];
     let mut dst_rotate_code = NO_ROTATE;
-    let mut max_chars_count = 0;
+    let mut max_words_count = 0;
 
     for rotate_code in rotate_code_list {
         // 画像の回転
@@ -37,15 +39,34 @@ fn main() {
             .unwrap_or_else(|code| panic!("{}", code));
         let ocr_str = ocr.run(&dst_img, 0, 0).unwrap();
 
-        println!("回転角度: {}° 文字数: {} 認識した文字 =>「{}」", angle_int, ocr_str.chars().count(), ocr_str);
-        if ocr_str.chars().count() > max_chars_count {
-            max_chars_count = ocr_str.chars().count();
+        // mecabを使った形態素解析
+        let mecab_env = env::var("EXTEND_MECAB_DICTIONARY");
+        let mecab_arg = match mecab_env {
+            Ok(path) => "-d ".to_string() + &path,
+            Err(err) => {
+                panic!("mecabの拡張辞書の取得に失敗しました。デフォルトの辞書を使用します。 Message: {}", err)
+            }
+        };
+        let mecab = Tagger::new(mecab_arg);
+        let parse_result = mecab.parse_str(ocr_str.clone());
+        let mut result_lines: Vec<&str> = parse_result.trim().split("\n").collect();
+        result_lines.remove(result_lines.len() - 1);
+
+        let recognizable_words = result_lines.iter().filter(|line| {
+            let regex = regex::Regex::new(r"\t|,").unwrap();
+            let word_info: Vec<&str> = regex.split(line).collect();
+            word_info[0].chars().count() >= 2 // 2文字以上であること
+        });
+
+        println!("回転角度: {}° 認識した文字数: {:?} 認識した文字 =>「{}」", angle_int, recognizable_words.clone().count(), ocr_str);
+        if recognizable_words.clone().count() > max_words_count {
+            max_words_count = recognizable_words.count();
             dst_rotate_code = rotate_code;
         }
     }
 
     let angle_at_max = rotate_code_to_angle_int(dst_rotate_code);
-    println!("認識できた文字数が最も多かった角度: {}°, 認識した文字数: {}", angle_at_max, max_chars_count);
+    println!("認識できた文字数が最も多かった角度: {}°, 認識した単語数: {}", angle_at_max, max_words_count);
 
     // 認識できた文字数が1番多かった角度へ回転させる
     let result_img = if dst_rotate_code != NO_ROTATE {
